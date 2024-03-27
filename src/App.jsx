6fee4effdd602688ee2dfe35d0342201e3b30652
Fardeen Ali <fardeenali83@gmail.com>
2024-03-28 02:47:50 +0530

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
-const App = () => {
-  return <div>Hello world!</div>;
-};
+// import React from 'react'
+import Cart from "./pages/Cart";
+import Home from "./pages/home"
+import Login from "./pages/Login";
+import ProductItem from "./pages/ProductItem";
+import ProductPage from "./pages/ProductPage";
+import Register from "./pages/Register";
+import WApp from "./pages/whatsapp";
 
-export default App;
+const App = () => {
+  return <Cart/>;
+};
+export default App
